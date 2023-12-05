4024892b8c174d50cf8fb1d03b7a7e754ab1634e
Ethan Harlig <ethan.harlig@gmail.com>
2023-12-04 19:52:19 -0800

    runnable test

--- generator/create_day.py
+++ generator/create_day.py
@@ -37,6 +37,20 @@ class DaySolution(Solution):
     def solve_part_two(self) -> str:
         print(self.input_data)
         return str("change me pls")
+
+
+if __name__ == "__main__":
+    today_solution = DaySolution("input0.txt")
+    print("running part one")
+    part_one = today_solution.solve_part_one()
+    print("part one results:")
+    print(part_one)
+
+    # enable once part one solved
+    # print("running part two")
+    # part_two = today_solution.solve_part_two()
+    # print("part two results:")
+    # print(part_two)
 """
         with open(os.path.join(path, "solution.py"), "w") as file:
             file.write(boilerplate_code)
--- solutions/days/2/solution.py
+++ solutions/days/2/solution.py
@@ -1,4 +1,3 @@
-
 from solutions.solution import Solution
 
 
@@ -10,3 +9,17 @@ class DaySolution(Solution):
     def solve_part_two(self) -> str:
         print(self.input_data)
         return str("change me pls")
+
+
+if __name__ == "__main__":
+    today_solution = DaySolution("input0.txt")
+    print("running part one")
+    part_one = today_solution.solve_part_one()
+    print("part one results:")
+    print(part_one)
+
+    # enable once part one solved
+    # print("running part two")
+    # part_two = today_solution.solve_part_two()
+    # print("part two results:")
+    # print(part_two)
